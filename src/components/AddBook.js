b621594da892983f40f38a036f7720a882e1923b
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-28 20:45:33 -0300

    JS

---
+++ src/components/AddBook.js
@@ -0,0 +1,17 @@
+import React from "react";
+import BookForm from './BookForm';
+
+const AddBook = () => {
+    const handleOnSubmit = (book) => {
+        console.log(book);
+    }
+
+    return(
+        <React.Fragment>
+            <BookForm handleOnSubmit={handleOnSubmit} />
+        </React.Fragment>
+    );
+};
+
+export default AddBook;
+
