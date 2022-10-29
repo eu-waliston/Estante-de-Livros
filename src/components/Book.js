22e3639741ce65daac3341d9d41d36e9ade6a224
Waliston Euripedes <goneisgone@hotmail.com>
2022-10-29 07:54:36 -0300

    JS

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Card } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const Book = ({
     id,
@@ -10,6 +11,7 @@ const Book = ({
     date,
     handleRemoveBook
 }) => {
+    const history = useHistory();
     return (
         <Card style={{ width: '18rem' }} className="book">
             <Card.Body>
@@ -22,7 +24,7 @@ const Book = ({
                 <div>Date: {new Date(date).toDateString()}</div>
             </div>
             
-            <Button variant="primary">Edit</Button>{' '}
+            <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>Edit</Button>{' '}
             <Button variant="danger" onClick={() => handleRemoveBook(id)}>Delete</Button>
 
             </Card.Body>
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,26 +1,45 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import AddBook from '../components/AddBook';
-import BookList from '../components/BookList';
-import useLocalStorage from "../hooks/useLocalStorage";
-
+import BooksList from '../components/BookList';
+import useLocalStorage from '../hooks/useLocalStorage';
+import EditBook from '../components/EditBooks';
 
 const AppRouter = () => {
-    const [books, setBooks] = useLocalStorage('books', [])
+    const [books, setBooks] = useLocalStorage('books', []);
+
     return (
         <BrowserRouter>
             <div>
                 <Header />
                 <div className="main-content">
                     <Switch>
-                        <Route render={(props) => (<BookList {...props} books={books} setBooks={setBooks} />)} path="/" exact={true} />
-                        <Route render={(props) => (<AddBook {...props} books={books} setBooks={setBooks} />)} path="/add" />
+                        <Route
+                            render={(props) => (
+                                <BooksList {...props} books={books} setBooks={setBooks} />
+                            )}
+                            path="/"
+                            exact={true}
+                        />
+                        <Route
+                            render={(props) => (
+                                <AddBook {...props} books={books} setBooks={setBooks} />
+                            )}
+                            path="/add"
+                        />
+                        <Route
+                            render={(props) => (
+                                <EditBook {...props} books={books} setBooks={setBooks} />
+                            )}
+                            path="/edit/:id"
+                        />
+                        <Route component={() => <Redirect to="/" />} />
                     </Switch>
                 </div>
             </div>
         </BrowserRouter>
-    )
-}
+    );
+};
 
 export default AppRouter;
